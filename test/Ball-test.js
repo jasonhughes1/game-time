6f888f2a504f77f69e4579b06b365f758e98b5bc
Jason Hughes <jasonhughes088@gmail.com>
2017-08-24 15:32:39 -0600

    fixes after eval

--- test/Ball-test.js
+++ test/Ball-test.js
@@ -1,8 +1,9 @@
 let chai = require('chai');
 let assert = chai.assert;
 
-let Ball = require('../lib/Ball.js');
-
+const Ball = require('../lib/Ball.js');
+const Paddle = require('../lib/Paddle.js')
+let ball;
 
 describe('ball test', function() {
   it('should be a function', function() {
@@ -37,11 +38,29 @@ describe('ball test', function() {
     assert.equal(typeof ball.bounceWalls, 'function');
   })
 
+  it('Ball should change directions when it hits a wall', function() {
+    const ball = new Ball (495, 495, 3, 3);
+    let canvas = { width: 900};
+
+    assert.equal(ball.x, 495)
+    assert.equal(ball.y, 495)
+    assert.equal(ball.moveX, 3)
+
+    ball.x = 901
+    ball.bounceWalls(canvas);
+    assert.equal(ball.moveX, -3)
+
+  })
+
   it('Ball should have a bounce off paddle method', function() {
-    const ball = new Ball ();
+    const ball = new Ball (11, 11);
+    const paddle = new Paddle(10, 10, 50);
 
-    assert.equal(typeof ball.bouncePaddle, 'function');
-
+    ball.bouncePaddle(paddle)
+    assert.equal(ball.x, 11)
+    assert.equal(ball.y, 11)
+    assert.equal(paddle.x, 10)
+    assert.equal(paddle.y, 10)
   })
 
   it('Ball should have the ability to reset', function () {
